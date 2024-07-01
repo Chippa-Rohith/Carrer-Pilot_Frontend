43da653ca8ee37e249057b363cc0d88a7b291f93
Chippa Rohith <64948774+Chippa-Rohith@users.noreply.github.com>
2024-07-01 13:13:55 +0530

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,77 @@
+import { useContext } from 'react'
+import { Logout } from '../utils/functions'
+import { store } from '../utils/store'
+
+
+const Layout=({children})=> {
+
+    const {state:{user}}=useContext(store)
+
+    const username=user?.UserName? user.UserName:"Dummy Username"
+    const firstname=user?.FirstName? user.FirstName:"Dummmy"
+    const lastname=user?.LastName? user.LastName:"name"
+    const name=firstname+" "+lastname
+    const email=user?.Email? user.Email:"Dummy@email.com"
+    const contact=user?.PhoneNumber? user.PhoneNumber:"dummycontact"
+
+  return (
+    <div className='layout'>
+        <div className='header'>
+            <div className="brand">
+                CarrerPiolt
+            </div>
+            <div className="rightNav">
+                <div className="Avatar">
+                    <img src={`https://api.dicebear.com/6.x/pixel-art/jpg?seed=${username}`} alt="user"/>
+                    <div className="text">{username}</div>
+                </div>
+                <ul className='sideBar'>
+                </ul>
+                <div className='rightItem'>
+                    <div className='logoutButton' onClick={Logout}>
+                        <div className="text">Logout</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="bodyHolder">
+            <div className='leftSideBar'>
+                <div class="profile-card">
+	<div className="profile-card-header">
+		<div className="profile-image">
+            <img style={{width:'100%', borderRadius:'10%' }} src={`https://api.dicebear.com/6.x/pixel-art/jpg?seed=${username}&backgroundColor=ffdfbf`} alt="user"/>
+        </div>
+
+		<div className="profile-info">
+			<h3 className="profile-name">{name}</h3>
+		</div>
+	</div>
+
+	<div className="profile-card-body">
+		<div className="status">
+        <li>
+				<span className="status-value">Username: </span>
+				<span className="status-text">{username}</span>
+			</li>
+			<li>
+				<span className="status-value">Email: </span>
+				<span className="status-text">{email}</span>
+			</li>
+
+			<li>
+				<span className="status-value">Contact: </span>
+				<span className="status-text">{contact}</span>
+			</li>
+		</div>
+	</div>
+</div>
+            </div>
+            <div className='mainContent'>
+                {children}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/utils/network.js
@@ -0,0 +1,22 @@
+export const CloudinaryUrl= "https://api.cloudinary.com/v1_1/dmileakzs/upload"
+
+export const BackendURL= "https://carrer-pilot-backend-production.up.railway.app"
+
+export const AllJobs=BackendURL + "/getAllJobsList";
+
+export const AppliedJobsUrl= BackendURL +"/getJobsAppliedByUser"
+
+export const PostedJobsUrl= BackendURL +"/getJobPostedByRecruiter"
+
+export const AppliedApplicantsUrl= BackendURL +"/appliedApplicants"
+
+export const LoginUrl = BackendURL + "/login"
+
+export const IsAuthorizedUrl = BackendURL + "/isAuthorized"
+
+export const CreateUserUrl = BackendURL + "/signup"
+
+export const AddJobUrl= BackendURL +"/addJobDetails"
+
+export const ApplyJobUrl= BackendURL +"/applyJob"
+
